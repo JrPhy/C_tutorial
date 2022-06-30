e05c2aca834f08a9f70219ea14ee6f6177ead148
JrPhy <j0958322080@gmail.com>
2022-06-30 08:37:24 +0800

    Update server.c

--- server.c
+++ server.c
@@ -14,9 +14,8 @@
  
 // Semaphore variables
 sem_t x, y;
-int a = 2;
 pthread_t tid;
-pthread_t readerthreads[a];
+pthread_t readerthreads[100];
 int readercount = 0;
  
 // Reader Function
@@ -34,7 +33,7 @@ void* reader(void* param)
  
     printf("\n%d CTB is inside\n", readercount);
  
-    sleep(20);
+    //sleep(20);
  
     // Lock the semaphore
     sem_wait(&x);
@@ -52,21 +51,37 @@ void* reader(void* param)
     pthread_exit(NULL);
 }
 // Driver Code
+int timeout_recv(int fd, char *buf, int len,  int nsec)
+{
+    struct timeval timeout;
+    timeout.tv_sec = nsec;
+    timeout.tv_usec = 0;
+
+    printf("timeout_recv called, timeout %d seconds\n", nsec);
+
+    if (setsockopt(fd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout)) < 0) {
+        perror("setsockopt error");
+        exit(1);
+    }
+
+    int n = recv(fd, buf, len, 0);
+
+    return n;
+}
+
 int main()
 {
     // Initialize variables
-    int serverSocket, newSocket, port = 0;
+    int serverSocket, newSocket, port = 0, i = 0;
     struct sockaddr_in serverAddr;
     struct sockaddr_storage serverStorage;
-    char buf[4096], ip[16] = "-1";
-    char *retPort, *retIP, *p;
- 
+
     socklen_t addr_size;
     sem_init(&x, 0, 1);
     sem_init(&y, 0, 1);
  
     serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_port = htons(8989);
  
@@ -83,34 +98,48 @@ int main()
     else printf("Error\n");
  
     // Array for thread
-    int thread_num = 0, thread_total = 1;
+    int thread_num = 0, thread_total = 10, status = 0, nread = -1, kkk = 0;
     pthread_t tid[thread_total];
 
     while (1) {
         addr_size = sizeof(serverStorage);
- 
         // Extract the first
         // connection in the queue
         newSocket = accept(serverSocket,
                            (struct sockaddr*)&serverStorage,
                            &addr_size);
-        int status = 0;
+                           
         if (pthread_create(&readerthreads[thread_num++], NULL,
                             reader, &newSocket) != 0)
             // Error in creating thread
         {
             printf("Failed to create thread\n");
             status = -1;
-            send(newSocket, &status, sizeof(status), 0);
         }
         else
         {
             printf("create thread sucessfully\n");
             status = 1;
-            send(newSocket, &status, sizeof(status), 0);
+        }
+        if( send(newSocket, &status, sizeof(status), 0) < 0)
+        {
+            puts("Send failed");
+            return 1;
+        }
+        while(recv(newSocket, &kkk, sizeof(kkk), 0) > 0) 
+        {
+            if(kkk%10000000==0) printf("%d\n", kkk);
+        }
+        //若數量已滿，client 端直接退出
+        //log: 使用者，連上時間，command
+        nread = timeout_recv(serverSocket, buf, sizeof(buf), 10);
+        if (nread == 0) {
+            printf("time out\n");
+            shutdown(serverSocket, SHUT_RDWR);
+            break;
         }
         if (thread_num >= thread_total) {
-            // Update i
+            // Update thread_num
             thread_num = 0;
             printf("Server is full, please wait\n");
             while (thread_num < thread_total) {
@@ -120,11 +149,9 @@ int main()
                 // thread terminates
                 pthread_join(readerthreads[thread_num++], NULL);
             }
- 
-            // Update i
+            // Update thread_num
             thread_num = 0;
         }
     }
- 
     return 0;
 }
